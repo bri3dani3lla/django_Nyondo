3a86f096dec5eb58fbbb0bf26ddeafa35d0ca44c
bri3dani3lla <briedaniella927@gmail.com>
2026-06-09 11:56:46 +0300

    add Procfile for render

--- hammerstock/hammerstock/settings.py
+++ hammerstock/hammerstock/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-p^^4vay+!!&1(kgn!wjxgefa+*hz@abt0jlr5)5!3ycf7uxjtq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.onrender.com',
+]
 
 
 # Application definition
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,5 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = (
+    'whitenoise.storage.CompressedManifestStaticFilesStorage'
+) 
 # redirect to login page if user is not logged in
 LOGIN_URL = '/login/'
